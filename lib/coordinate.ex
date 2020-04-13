5d76a9d03acaaf6d644e0d4ed396e5c832889999
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-04-14 01:00:26 +0600

    fixed formatting

--- lib/coordinate.ex
+++ lib/coordinate.ex
@@ -6,14 +6,16 @@ defmodule IslandsEngine.Coordinate do
 
   def guess(coordinate),
     do: Agent.update(coordinate, fn state -> Map.put(state, :guessed?, true) end)
-  def set_in_island(coordinate, value) when is_atom value do
+
+  def set_in_island(coordinate, value) when is_atom(value) do
     Agent.update(coordinate, fn state -> Map.put(state, :in_island, value) end)
   end
-  
+
   def guessed?(coordinate), do: Agent.get(coordinate, fn state -> state.guessed? end)
   def island(coordinate), do: Agent.get(coordinate, fn state -> state.in_island end)
   def in_island?(coordinate), do: if(island(coordinate) == :none, do: false, else: true)
   def hit?(coordinate), do: in_island?(coordinate) and guessed?(coordinate)
 
-  def to_string(coordinate), do: "(in_island:#{island(coordinate)}, guessed:#{guessed?(coordinate)})"
+  def to_string(coordinate),
+    do: "(in_island:#{island(coordinate)}, guessed:#{guessed?(coordinate)})"
 end
--- lib/island.ex
+++ lib/island.ex
@@ -1,7 +1,8 @@
 defmodule IslandsEngine.Island do
   alias IslandsEngine.Coordinate
   def start_link(), do: Agent.start_link(fn -> [] end)
-  def replace_coordinates(island, new_coordinates) when is_list new_coordinates do
+
+  def replace_coordinates(island, new_coordinates) when is_list(new_coordinates) do
     Agent.update(island, fn _state -> new_coordinates end)
   end
 
@@ -11,16 +12,14 @@ defmodule IslandsEngine.Island do
     |> Enum.all?(fn coord -> Coordinate.hit?(coord) end)
   end
 
-
   def to_string(island) do
     "[" <> coordinate_strings(island) <> "]"
   end
 
-
   defp coordinate_strings(island) do
     island
     |> Agent.get(fn state -> state end)
-    |> Enum.map( fn coord -> Coordinate.to_string(coord) end)
+    |> Enum.map(fn coord -> Coordinate.to_string(coord) end)
     |> Enum.join(", ")
   end
 end
