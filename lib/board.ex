83339c68ef7709ba7c129f73c2953f27fc91b7bf
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-04-14 01:21:29 +0600

    fixed formatting

--- lib/board.ex
+++ lib/board.ex
@@ -2,35 +2,38 @@ defmodule IslandsEngine.Board do
   alias IslandsEngine.Coordinate
   @letters ~W(a b c d e f g h)
   @numbers [1, 2, 3, 4, 5, 6, 7, 8]
-  def start_link(), do: Agent.start_link( fn -> initialize_board() end)
+  def start_link(), do: Agent.start_link(fn -> initialize_board() end)
+
   def get_coordinate(board, key) when is_atom(key) do
     Agent.get(board, fn board -> board[key] end)
   end
-  
+
   def guess_coordinate(board, key) do
     get_coordinate(board, key)
-    |> Coordinate.guess 
+    |> Coordinate.guess()
   end
+
   def coordinate_hit?(board, key) do
     get_coordinate(board, key)
-    |> Coordinate.hit? 
+    |> Coordinate.hit?()
   end
 
   def coordinate_island(board, key) do
     get_coordinate(board, key)
-    |> Coordinate.island
+    |> Coordinate.island()
   end
 
   def to_string(board) do
     "%{" <> string_body(board) <> "}"
   end
+
   defp string_body(board) do
     Enum.reduce(keys(), "", fn key, acc ->
-        coord = get_coordinate(board, key)
-        acc <> "#{key} => #{Coordinate.to_string(coord)},\n"
+      coord = get_coordinate(board, key)
+      acc <> "#{key} => #{Coordinate.to_string(coord)},\n"
     end)
   end
-  
+
   defp keys() do
     for letter <- @letters, number <- @numbers do
       String.to_atom("#{letter}#{number}")
@@ -38,10 +41,9 @@ defmodule IslandsEngine.Board do
   end
 
   defp initialize_board do
-    Enum.reduce(keys(), %{}, fn (key, board) -> 
-      {:ok, coord} = Coordinate.start_link
+    Enum.reduce(keys(), %{}, fn key, board ->
+      {:ok, coord} = Coordinate.start_link()
       Map.put_new(board, key, coord)
     end)
   end
-
 end
